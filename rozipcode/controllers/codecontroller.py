b03ed90c73ee46a0123052df03777ef0154452f2
Mihai Costea <mihai.andrei.costea@icloud.com>
2016-09-20 17:34:28 +0300

    Add support for reverse order street names

--- rozipcode/controllers/codecontroller.py
+++ rozipcode/controllers/codecontroller.py
@@ -11,8 +11,9 @@ class CodeController:
         matches = [x.__dict__ for x in self.__codes if (True if county is None else (county is not None and x.county is not None and county.lower() in x.county.lower())) and
                    (True if locality is None else (locality is not None and x.locality is not None and locality.lower() in x.locality.lower())) and
                    (True if sector is None else (sector is not None and x.sector is not None and str(sector).lower() in str(x.sector).lower())) and
-                   # TODO: Street names are reverse word order in data. Make sure to check for ALL words not just partial match
-                   (True if street is None else (street is not None and x.street is not None and street.lower() in x.street.lower())) and
+                   (True if street is None else (street is not None and x.street is not None and
+                                                 (street.lower() in x.street.lower() or
+                                                  all(word in x.street.lower() for word in street.lower().split())))) and
                    # TODO: House number check does not take into consideration house number ranges
                    (True if house_number is None else (house_number is not None and x.house_number is not None and house_number.lower() in x.house_number.lower()))
                    ]
--- rozipcode/tests/test_code_controller_bucharest.py
+++ rozipcode/tests/test_code_controller_bucharest.py
@@ -69,6 +69,18 @@ class TestCodeControllerBucharest(unittest.TestCase):
         self.assertEqual(matches[5]["zip"], "011426")
         self.assertEqual(matches[6]["zip"], "011427")
 
+    def test_find_by_address_street_only_reverse(self):
+        matches = self.controller.find_by_address(street="Emanoil Porumbaru")
+
+        self.assertEqual(len(matches), 7)
+        self.assertEqual(matches[0]["zip"], "011421")
+        self.assertEqual(matches[1]["zip"], "011422")
+        self.assertEqual(matches[2]["zip"], "011423")
+        self.assertEqual(matches[3]["zip"], "011424")
+        self.assertEqual(matches[4]["zip"], "011425")
+        self.assertEqual(matches[5]["zip"], "011426")
+        self.assertEqual(matches[6]["zip"], "011427")
+
     def test_find_by_address_house_number_only(self):
         matches = self.controller.find_by_address(house_number="nr. 21-T")
 
